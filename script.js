7d9b843c2e68776dab7c295091e8f3824e67a4a6
sujwal <sumanbh@hotmail.com>
2020-02-07 21:41:53 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var button = document.querySelector('button');
+  
+    button.addEventListener('click', function() {
+      alert("Please sign in using your username and password");
+    });
+  }
