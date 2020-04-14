3d0f63f12b718193d5bc153d87b6264c26d15724
Suman <sumanbh@hotmail.com>
2020-04-14 09:56:45 -0600

    changes

--- Test-DBconnection.js
+++ Test-DBconnection.js
@@ -11,10 +11,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: true});
 //app.use(bodyParser.urlencoded({ extended: true }));
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-var SaskModel = require('./models/sask.model')
-var UserModel = require('./models/user.model')
-var NewcomerModel = require('./models/apply.model')
-var RenewModel = require('./models/renew.model')
+
 //Set up default mongoose connection
 //var mongoDB = process.env.MONGO_CONNECT_URI
 var mongoDB = 'mongodb+srv://mongo:P&N/,eqCtR99h+9@cluster0-jgw2s.azure.mongodb.net/test?retryWrites=true&w=majority';
@@ -36,46 +33,23 @@ app.get('/index', (req, res) => {
   res.render('index', {});
 
 });
-app.get('/member', (req, res) => {
-  res.render('member',{})
+app.get('/register', (req, res) => {
+  res.render('register',{})
 });
-app.get('/application', (req, res) => {
-  res.render('application',{})
+app.get('/signin', (req, res) => {
+  res.render('signin',{})
 });
-app.get('/event', (req, res) => {
-  res.render('event',{})
-});
-app.get('/pastevent', (req, res) => {
-  res.render('pastevent',{})
-});
-app.get('/upcoming', (req, res) => {
-  res.render('upcoming',{})
-});
-app.get('/renew', (req, res) => {
-  res.render('renew',{})
-});
+
+
 app.get('/contact', (req,res) =>{
   res.render('contact',{})
 });
-app.get('/About', (req,res) =>{
-  res.render('About',{})
-});
-app.get('/gallery', (req,res) =>{
-  res.render('gallery',{})
-});
 
 
-app.post("/contact", urlencodedParser, (req, res) => {
-  var myData = new UserModel(req.body);
-  myData.save()
-    .then(item => {
-      res.redirect("/")
-    })
-    .catch(err => {
-      res.status(400).send("unable to save to database");
-    });
-});
-app.post("/apply", urlencodedParser, (req, res) => {
+
+
+
+app.post("/register", urlencodedParser, (req, res) => {
   var myData = new NewcomerModel(req.body);
   myData.save()
     .then(item => {
@@ -85,7 +59,7 @@ app.post("/apply", urlencodedParser, (req, res) => {
       res.status(400).send("unable to save to database");
     });
 });
-app.post("/renew", urlencodedParser, (req, res) => {
+app.post("/signin", urlencodedParser, (req, res) => {
   var myData = new RenewModel(req.body);
   myData.save()
     .then(item => {
